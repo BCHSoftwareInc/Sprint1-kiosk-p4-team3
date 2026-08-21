209288ee6a4b1d683b11895254cd287ecefeb38c
Edsch9086 <edschaulat@gmail.com>
2026-08-21 10:17:39 -0700

    example code to grab email and name, no sanitization

--- java/KiosApp.java
+++ java/KiosApp.java
@@ -8,10 +8,15 @@ public class KioskApp {
         System.out.println("========================================");
         System.out.println("      BCH ENTERPRISE VISITOR KIOSK      ");
         System.out.println("========================================");
-        System.out.prinln("                                         ");
-        System.out.prinln("-------------give info here--------------");
-        System.out.prinin("full name-")
+        System.out.println("                                         ");
+        System.out.println("-------------give info here--------------");
+        System.out.println("full name-")
         String name = scanner.nextLine();
+        System.out.println("email addr")
+        String email = scanner.nextLine();
+        System.out.println("info:")
+        System.out.println(name)
+        System.out.println(email)
         // SE: Use scanner.nextLine() to capture 4 data fields
         // SE: Use System.out.println() to render the ASCII badge
     }
