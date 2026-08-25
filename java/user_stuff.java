201daa45f947d8e1486241d54dbc0de20cf00eef
dan <dtjan177@learner.dvusd.org>
2026-08-25 11:22:44 -0700

    added more to the max

--- java/user_stuff.java
+++ java/user_stuff.java
@@ -19,14 +19,14 @@ public class user_stuff
         String acess = scanner.nextLine();
         System.out.println("|Issuer:                                 |");
         String issuer = scanner.nextLine();
-        System.out.println("-------------------------------");
-        System.out.println("|   Apex Entertaninment pass  |");
-        System.out.println("-------------------------------");
-        System.out.println("| Attendee:" + String.format("%-19s", name) +"|");
-        System.out.println("| Contact :" + String.format("%-19s", email) +"|");
-        System.out.println("| Org     :" + String.format("%-19s", org) +"|");
-        System.out.println("| Acess   :" + String.format("%-19s", acess) +"|");
-        System.out.println("| Issuer  :" + String.format("%-19s", issuer)+"|");
+        System.out.println("-------------------------------------");
+        System.out.println("|   Apex Entertaninment pass        |");
+        System.out.println("-------------------------------------");
+        System.out.println("| Attendee:" + String.format("%-25s", name) +"|");
+        System.out.println("| Contact :" + String.format("%-25s", email) +"|");
+        System.out.println("| Org     :" + String.format("%-25s", org) +"|");
+        System.out.println("| Acess   :" + String.format("%-25s", acess) +"|");
+        System.out.println("| Issuer  :" + String.format("%-25s", issuer)+"|");
         System.out.println("_______________________________");
 }
 }
