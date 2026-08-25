77bf2e373a07163f21f7bc74c934606efa4330b1
dan <dtjan177@learner.dvusd.org>
2026-08-25 11:14:22 -0700

    FIxed spacing 1 st draft

---
+++ java/user_stuff.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class user_stuff
+{
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("|========================================|");
+        System.out.println("|      BCH ENTERPRISE VISITOR KIOSK      |");
+        System.out.println("|========================================|");
+        System.out.println("|------------give info here--------------|");
+        System.out.println("|Attendee:                               |");
+        String name = scanner.nextLine();
+        System.out.println("|email address:                          |");
+        String email = scanner.nextLine();
+        System.out.println("|org:                                    |");
+        String org = scanner.nextLine();
+        System.out.println("|acess:                                  |");
+        String acess = scanner.nextLine();
+        System.out.println("|issuer:                                 |");
+        String issuer = scanner.nextLine();
+        System.out.println("-------------------------------");
+        System.out.println("|   Apex Entertaninment pass  |");
+        System.out.println("-------------------------------");
+        System.out.println("| Attendee:" + String.format("%-19s", name) +"|");
+        System.out.println("| Contact :" + String.format("%-19s", email) +"|");
+        System.out.println("| Org     :" + String.format("%-19s", org) +"|");
+        System.out.println("| Acess   :" + String.format("%-19s", acess) +"|");
+        System.out.println("| Issuer  :" + String.format("%-19s", issuer)+"|");
+        System.out.println("_______________________________");
+}
+}
