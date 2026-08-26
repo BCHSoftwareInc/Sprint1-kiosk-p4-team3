a396bfb576d427f5b782dadeb160b2ba571e769f
Edsch9086 <edschaulat@gmail.com>
2026-08-26 11:34:53 -0700

    Fix spelling, unclear instructions, and resource leak.

--- java/KioskApp.java
+++ java/KioskApp.java
@@ -8,25 +8,37 @@ public class KioskApp
         System.out.println("|========================================|");
         System.out.println("|      BCH ENTERPRISE VISITOR KIOSK      |");
         System.out.println("|========================================|");
-        System.out.println("|------------give info here--------------|");
-        System.out.println("|Attendee:                               |");
-        String name = scanner.nextLine();
-        System.out.println("|Email address:                          |");
-        String email = scanner.nextLine();
-        System.out.println("|Org:                                    |");
-        String org = scanner.nextLine();
-        System.out.println("|Acess:                                  |");
-        String acess = scanner.nextLine();
-        System.out.println("|Issuer:                                 |");
-        String issuer = scanner.nextLine();
-        System.out.println("-------------------------------------");
-        System.out.println("|   Apex Entertaninment pass        |");
-        System.out.println("-------------------------------------");
-        System.out.println("| Attendee:" + String.format("%-25s", name) +"|");
-        System.out.println("| Contact :" + String.format("%-25s", email) +"|");
-        System.out.println("| Org     :" + String.format("%-25s", org) +"|");
-        System.out.println("| Acess   :" + String.format("%-25s", acess) +"|");
-        System.out.println("| Issuer  :" + String.format("%-25s", issuer)+"|");
-        System.out.println("_____________________________________");
-}
+        System.out.println("Please enter your registration details.     ");
+        System.out.println();
+
+        // get info from user
+        System.out.print("Enter First/Last Name: ");
+        String name = scanner.nextLine().trim(); // trim() removes leading and trailing spaces, tabs, and return chars
+        
+        System.out.print("Enter Organization: ");
+        String org = scanner.nextLine().trim();
+
+        System.out.print("Enter Contact Email: ");
+        String email = scanner.nextLine().trim();
+
+        System.out.print("Enter Access Level: ");
+        String access = scanner.nextLine().trim();
+
+        
+        // print pass
+        // %-29.29s |%n is what dynamically changes padding. println cant do that, hence why use printf
+        System.out.println();
+        System.out.println("+------------------------------------------+");
+        System.out.println("|         APEX ENTERTAINMENT PASS          |");
+        System.out.println("+------------------------------------------+");
+        System.out.println("|                                          |");
+        System.out.printf("| ATTENDEE : %-29.29s |%n", name);
+        System.out.printf("| ORG      : %-29.29s |%n", org);
+        System.out.printf("| CONTACT  : %-29.29s |%n", email);
+        System.out.printf("| ACCESS   : %-29.29s |%n", access);
+        System.out.println("| ISSUER   : BCH Software Terminal System  |");
+        System.out.println("+------------------------------------------+");
+
+        scanner.close();
+    }
 }
